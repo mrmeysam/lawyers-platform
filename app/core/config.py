ed523442aa2d69dddf40b9a0e730af456bca56a6
mrmeysam <40243597+mrmeysam@users.noreply.github.com>
2026-06-25 20:59:13 +0330

    pull

--- app/core/config.py
+++ app/core/config.py
@@ -1,10 +1,16 @@
 from pydantic_settings import BaseSettings
+import logging
 
 class Settings(BaseSettings):
     DATABASE_URL: str
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # این خط را اضافه کنید:
+        logging.info(f"✅ DATABASE_URL from config: {self.DATABASE_URL}")
 
     class Config:
         env_file = ".env"
